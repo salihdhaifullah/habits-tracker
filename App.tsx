ed5c8d40ac7b0a6d8f43acb065d18e1bd1cd47c2
salihdhaifullah <salihdhaifullah@gmail.com>
2024-03-29 23:49:39 +0200

    add nativewind

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import Constants from 'expo-constants'
+
+export default function App() {
+  return (
+    <View className="flex-1 bg-red-300 p-2 justify-center items-center" style={{marginTop: Constants.statusBarHeight}}>
+      <Text className='text-6xl text-center text-blue-700 border-green-600 border-2 bg-white p-4 rounded-md'>HABITUP</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
-  
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        "inline-import", 
-        {
-          "extensions": [".sql"],
-          "loaders": {
-            ".sql": (source) => {
-              const filePath = path.resolve(__dirname, source);
-              const fileContent = fs.readFileSync(filePath, 'utf8');
-              return `export default ${JSON.stringify(fileContent)};`;
-            }
-          }
-        }
-      ]
-    ]
+    plugins: ["nativewind/babel"],
   };
 };
